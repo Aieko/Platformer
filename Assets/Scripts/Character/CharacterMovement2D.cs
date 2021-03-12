797366d34a83ee44abeba7d569c54a68f8df10c0
Aieko <Aieko666@hotmail.com>
2021-03-12 17:29:18 +0300

    add TestLvl, MainCharacter and Enemy_instant
    Add several scripts
    main character:
    CharacterController
    CharacterMovement
    MeleeCombat
    Enemy_Instant
    Enemy (the basics code, which allow enemys be damaged by MainCharacter)
    
    Add test animations of MainCharacter and Enemy_Instant

---
+++ Assets/Scripts/Character/CharacterMovement2D.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement2D : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public Animator animator;
+
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal")* runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+    }
+
+   public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+      
+    }
+}
---
+++ Assets/Scripts/Character/MeleeCombat.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeCombat : MonoBehaviour
+{
+    public Animator animator;
+
+    public Transform attackPoint;
+    public LayerMask enemyLayers;
+
+    public float attackRange = 0.5f;
+    public int attackDamage = 40;
+
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && Time.time >=nextAttackTime)
+        {
+            Attack();
+            nextAttackTime = Time.time + 1f / attackRate;
+        }
+
+    }
+
+   void Attack()
+    {
+        //Attack Animation
+        animator.SetTrigger("Attack");
+
+        //Detect enemies in range of attack
+       Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy_Instant>().TakeDamage(attackDamage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if(attackPoint == null)
+         return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
+
+
